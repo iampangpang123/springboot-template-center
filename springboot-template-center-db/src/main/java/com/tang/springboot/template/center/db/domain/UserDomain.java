5dbf2a87f36e2c82876b564e47b60cfbc54ae9a9
iampangpang123 <iamispangpang@163.com>
2021-11-04 12:14:19 +0800

    1

---
+++ springboot-template-center-db/src/main/java/com/tang/springboot/template/center/db/domain/UserDomain.java
@@ -0,0 +1,9 @@
+package com.tang.springboot.template.center.db.domain;
+
+/**
+ * @description:
+ * @author: ttao
+ * @create: 2021-09-19 15:31
+ **/
+public interface UserDomain {
+}
---
+++ springboot-template-center-db/src/main/java/com/tang/springboot/template/center/db/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.tang.springboot.template.center.db.mapper;
+
+/**
+ * @description:
+ * @author: ttao
+ * @create: 2021-09-19 15:32
+ **/
+public interface UserMapper {
+}
